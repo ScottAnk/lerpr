3b9247cc8063dc84b2215284bd635ea1d58a0ad4
Scott Ankiewicz <scottfa.a@gmail.com>
2023-03-09 18:06:07 -0500

    test method for linking curve anchor points

--- src/pages/Editor/Editor.js
+++ src/pages/Editor/Editor.js
@@ -6,15 +6,16 @@ import ControlPanel from '../../components/ControlPanel/ControlPanel'
 export default function Resources() {
   const [user, setUser] = useState()
   // TODO this needs to be initialized to an empty array in deployment
+  const linkedPoint = { x: 400, y: 200, solid: true }
   const [curves, setCurves] = useState([
     [
       { x: 100, y: 100, solid: true },
       { x: 200, y: 500, solid: false },
       { x: 300, y: 100, solid: false },
-      { x: 400, y: 200, solid: true },
+      linkedPoint,
     ],
     [
-      { x: 400, y: 200, solid: true },
+      linkedPoint,
       { x: 100, y: 600, solid: false },
       { x: 600, y: 400, solid: false },
       { x: 600, y: 600, solid: true },
