534f35e95755fa3c2c75876fa8aa4cfe639a5909
Scott Ankiewicz <scottfa.a@gmail.com>
2023-03-09 14:50:13 -0500

    set up control point layout
    Co-Authored-By: Eddie-Hernandez eddie.hernandez115@gmail.com
    Co-Authored-By: Paul-Seabrook paulwarrenseabrook@gmail.com

--- src/components/ControlPoint/ControlPoint.js
+++ src/components/ControlPoint/ControlPoint.js
@@ -1,10 +1,17 @@
+import './ControlPoint.css'
+
+import Coordinate from '../Coordinate/Coordinate'
+
 export default function ControlPoint({ curve, number, setCurve }) {
   return (
-    <li>
-      <div style={{ display: 'grid' }}>
-        <span>curve {number}</span>
-      </div>
-      : list of x.y coordinates
+    <li className="ControlPointContainer">
+      <span className="CurveLabel">curve {number}</span>
+      <span className="XLabel">X</span>
+      <span className="YLabel">Y</span>
+      {/* : list of x.y coordinates */}
+      {curve.map((point, index) => (
+        <Coordinate key={index} point={point} />
+      ))}
     </li>
   )
 }
