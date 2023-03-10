745e86da034d1b288380146f53741a89a2a278d7
Scott Ankiewicz <scottfa.a@gmail.com>
2023-03-10 11:14:25 -0500

    align placeholder curves array with database format

--- src/pages/Editor/Editor.js
+++ src/pages/Editor/Editor.js
@@ -2,24 +2,25 @@ import React, { useState } from 'react'
 
 import Sandbox from '../../components/Sandbox/Sandbox'
 import ControlPanel from '../../components/ControlPanel/ControlPanel'
+import GradientDisplay from '../../components/GradientDisplay/GradientDisplay'
 
 export default function Resources() {
   const [user, setUser] = useState()
   // TODO this needs to be initialized to an empty array in deployment
   const linkedPoint = { x: 400, y: 200, solid: true }
   const [curves, setCurves] = useState([
-    [
-      { x: 100, y: 100, solid: true },
-      { x: 200, y: 500, solid: false },
-      { x: 300, y: 100, solid: false },
-      linkedPoint,
-    ],
-    [
-      linkedPoint,
-      { x: 100, y: 600, solid: false },
-      { x: 600, y: 400, solid: false },
-      { x: 600, y: 600, solid: true },
-    ],
+    {
+      startPoint: { x: 100, y: 100, solid: true },
+      endPoint: linkedPoint,
+      control1: { x: 200, y: 500, solid: false },
+      control2: { x: 300, y: 100, solid: false },
+    },
+    {
+      startPoint: linkedPoint,
+      endPoint: { x: 600, y: 600, solid: true },
+      control1: { x: 100, y: 600, solid: false },
+      control2: { x: 600, y: 400, solid: false },
+    },
   ])
 
   return (
