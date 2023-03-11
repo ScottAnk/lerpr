c6e15ac4a9da60c4e772005d143be28fd15b39de
Paul Truitt <paultruitt@Pauls-MacBook-Pro.local>
2023-03-10 22:04:33 -0500

    deleteOneCurve controller written, routed, tested in postman

--- controllers/api/curves.js
+++ controllers/api/curves.js
@@ -2,36 +2,49 @@ const Sandbox = require('../../models/sandbox')
 
 // Create
 async function createCurve(req, res, next) {
-    try {
-        const curve = req.body
-        const sandboxId = req.body.sandboxId
-        const sandbox = await Sandbox.findById(sandboxId)
-        sandbox.curves.push(curve)
-        return sandbox.save()
-            .then((sandbox) => {
-                res.status(201).json({ sandbox: sandbox })
-            })
-    } catch(error) {
-        console.log(error)
-        next(error)
-    }
+  try {
+    const curve = req.body
+    const sandboxId = req.body.sandboxId
+    const sandbox = await Sandbox.findById(sandboxId)
+    sandbox.curves.push(curve)
+    return sandbox.save().then((sandbox) => {
+      res.status(201).json({ sandbox: sandbox })
+    })
+  } catch (error) {
+    console.log(error)
+    next(error)
+  }
 }
 
 // Delete all curves (clear button)
 async function clearCurves(req, res, next) {
-    try {
-        const sandboxId = req.body.sandboxId
-        const sandbox = await Sandbox.findById(sandboxId)
-        sandbox.curves.pull({})
-        return sandbox.save()
-            .then(() => res.sendStatus(204))
-    } catch(error) {
-        console.log(error)
-        next(error)
-    }
+  try {
+    const sandboxId = req.body.sandboxId
+    const sandbox = await Sandbox.findById(sandboxId)
+    sandbox.curves.pull({})
+    return sandbox.save().then(() => res.sendStatus(204))
+  } catch (error) {
+    console.log(error)
+    next(error)
+  }
+}
+
+// Delete one curve
+async function deleteOneCurve(req, res, next) {
+  try {
+    const curveId = req.params.id
+    const sandboxId = req.body.sandboxId
+    const sandbox = await Sandbox.findById(sandboxId)
+    sandbox.curves.pull(curveId)
+    return sandbox.save().then(() => res.sendStatus(204))
+  } catch (error) {
+    console.log(error)
+    next(error)
+  }
 }
 
 module.exports = {
-    createCurve,
-    clearCurves,
+  createCurve,
+  clearCurves,
+  deleteOneCurve,
 }
--- routes/api/curves.js
+++ routes/api/curves.js
@@ -6,5 +6,7 @@ const curvesCtrl = require('../../controllers/api/curves')
 router.post('/', curvesCtrl.createCurve)
 // DELETE /api/curves/deleteAll
 router.delete('/deleteAll', curvesCtrl.clearCurves)
+// DELETE /api/curves/delete/:id
+router.delete('/delete/:id', curvesCtrl.deleteOneCurve)
 
 module.exports = router
