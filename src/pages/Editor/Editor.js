60b3f6d7393cdbc1842da8bcf4959740ceae1db0
Scott Ankiewicz <scottfa.a@gmail.com>
2023-03-12 15:47:31 -0400

    improve initialization of sandbox state

--- src/pages/Editor/Editor.js
+++ src/pages/Editor/Editor.js
@@ -12,17 +12,41 @@ export default function Editor({ setUser, user, openSignIn, setOpenSignIn }) {
   const exportRef = useRef()
   const [open, setOpen] = useState(false)
   const [openClearPrompt, setOpenClearPrompt] = useState(false)
-  const [sandbox, setSandbox] = useState({ name: 'yourSandBox' })
-  const [colorStart, setColorStart]=useState({r:198, g:153, b:128})
-  const [colorStop, setColorStop]=useState({r:162, g:181, b:221})
+  const linkedPoint = { x: 300, y: 300, solid: true }
+  const linkedPoint2 = { x: 600, y: 0, solid: true }
+  const [sandbox, setSandbox] = useState({
+    name: 'yourSandBox',
+    colorStart: { r: 198, g: 153, b: 128 },
+    colorStop: { r: 162, g: 181, b: 221 },
+    curves: [
+      {
+        startPoint: { x: 0, y: 600, solid: true },
+        endPoint: linkedPoint,
+        control1: { x: 100, y: 500, solid: false },
+        control2: { x: 200, y: 100, solid: false },
+      },
+      {
+        startPoint: linkedPoint,
+        endPoint: linkedPoint2,
+        control1: { x: 400, y: 100, solid: false },
+        control2: { x: 500, y: 500, solid: false },
+      },
+      {
+        startPoint: linkedPoint2,
+        endPoint: { x: 900, y: 0, solid: true },
+        control1: { x: 700, y: 100, solid: false },
+        control2: { x: 800, y: 500, solid: false },
+      },
+    ],
+  })
+  const [colorStart, setColorStart] = useState({ r: 198, g: 153, b: 128 })
+  const [colorStop, setColorStop] = useState({ r: 162, g: 181, b: 221 })
 
   // selecting specific curves
   const [selectedCurve, setSelectedCurve] = useState()
   const [deleteClass, setDeleteClass] = useState(false)
 
   // TODO this needs to be initialized to an empty array in deployment
-  const linkedPoint = { x: 300, y: 300, solid: true }
-  const linkedPoint2 = { x: 600, y: 0, solid: true }
   const [curves, setCurves] = useState([
     {
       startPoint: { x: 0, y: 600, solid: true },
@@ -44,17 +68,27 @@ export default function Editor({ setUser, user, openSignIn, setOpenSignIn }) {
     },
   ])
 
-
   return (
     <>
       <main className="Editor">
         <h1>Sandbox</h1>
 
         <div className="MainEditorContainer">
-          <Sandbox curves={curves} exportRef={exportRef}/>
+          <Sandbox curves={curves} exportRef={exportRef} />
           <div className="InnerEditorContainer">
-            <ControlPanel curves={curves} setCurves={setCurves} deleteClass={deleteClass} setDeleteClass={setDeleteClass} />
-            <GradientDisplay curves={curves} colorStart={colorStart} colorStop={colorStop} setColorStart={setColorStart} setColorStop={setColorStop} />
+            <ControlPanel
+              curves={curves}
+              setCurves={setCurves}
+              deleteClass={deleteClass}
+              setDeleteClass={setDeleteClass}
+            />
+            <GradientDisplay
+              curves={curves}
+              colorStart={colorStart}
+              colorStop={colorStop}
+              setColorStart={setColorStart}
+              setColorStop={setColorStop}
+            />
           </div>
         </div>
       </main>
